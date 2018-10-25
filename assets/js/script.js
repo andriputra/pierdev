432a237c9ec90830e3b3e45312de45f3d12ce1f7
andriputra <andriputra.job@gmail.com>
2018-10-26 00:45:51 +0700

    modification header fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,12 @@
-// $(document).ready(function(){
-//   var navHeight = $('#myHeader').offset().top;
-//   $(window).scroll(function(){
-//     if ($(window).scrollTop() > navHeight) {
-//       $("#myHeader").addClass("fixed");
-//       $("#myHeader").slideDown('200');
-//     }
-//     else {
-//       $("#myHeader").removeClass("fixed");
-//     }
-
-
-//    $( "#slider-range" ).slider({
-//       range: true,
-//       min: 0,
-//       max: 500,
-//       values: [ 75, 300 ],
-//       slide: function( event, ui ) {
-//         $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-//       }
-//     });
-//     $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-//       " - $" + $( "#slider-range" ).slider( "values", 1 ) );
-// 	});
-// });
+$(document).ready(function(){
+  var navHeight = $('#Header').offset().top;
+  $(window).scroll(function(){
+    if ($(window).scrollTop() > navHeight) {
+      $("#Header").addClass("fixed");
+      $("#Header").slideDown('200');
+    }
+    else {
+      $("#Header").removeClass("fixed");
+    }
+  });
+});
